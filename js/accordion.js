d9959cfa6f71b1cd3bf20641c3fdf29f84328edf
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-06-04 12:18:47 +0200

    fix accord bug, add proper lorems, some in line styling

--- js/accordion.js
+++ js/accordion.js
@@ -4,23 +4,27 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
 
-        for (j = 0; j < acc.length; j++){
-            
+        for (j = 0; j < acc.length; j++) {
+
             let panel = acc[j].nextElementSibling;
-            if (panel.style.maxHeight && acc.item(j) != this){
+            if (panel.style.maxHeight && acc.item(j) != this) {
                 panel.style.maxHeight = null;
-                
+                panel.style.overflow = "hidden";
+
             }
         };
 
-        
+
         this.classList.toggle("active");
 
         let panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
+            panel.style.overflow = "hidden";
         } else {
-            panel.style.maxHeight = (panel.scrollHeight + 20)+ "px";
+            panel.style.maxHeight = (panel.scrollHeight + 20) + "px";
+            //to ensure nothing cut off, but only after roll out!
+            setTimeout(() => {panel.style.overflow = "visible"}, 200);
         }
     });
 }
