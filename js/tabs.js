213a48213139e6b29752fc80774ffcbc93d6b6e0
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-06-06 22:45:13 +0200

    fixed multiple tabs on place it only, fucked all other pages :(

--- js/tabs.js
+++ js/tabs.js
@@ -1,22 +1,30 @@
-function openTabContent(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
+function openTabContent(evt, cityName, containerName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-    //TODO: should not kill tabs of other containers
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tab-button");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Get all elements with class="tabcontent" and hide them
+
+  //TODO: should not kill tabs of other containers
+  let currentContainer = document.getElementById(containerName);
+  tabcontent = currentContainer.querySelectorAll("." + "tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = currentContainer.querySelectorAll("." + "tab-button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  currentContainer.querySelector("#" + cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openDefaultTabs(evt) {
+  let defaults = document.querySelectorAll(".default-tab")
+  defaults.forEach(element => {
+    element.style.display = "block";
+  });
+}
